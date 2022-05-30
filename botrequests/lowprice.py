c013dbcfa82b1a5a946250f4af439e069da5e552
Stanislav Zhuravlev <suntoo@mail.ru>
2022-05-30 17:22:33 +0300

    lowprice - рабочая версия

--- botrequests/lowprice.py
+++ botrequests/lowprice.py
@@ -1,7 +1,7 @@
 import requests
 import json
-from datetime import datetime
 import os
+import random
 
 headers = {
         "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
@@ -10,6 +10,7 @@ headers = {
 
 
 def city_id(name_city):
+    '''def city_id - получаем id города, в котором будем искать отели'''
     url = "https://hotels4.p.rapidapi.com/locations/v2/search"
     querystring = {"query": name_city}
     response = requests.get(url, headers=headers, params=querystring)
@@ -24,104 +25,25 @@ def city_id(name_city):
             if name_city[0:3] in my_city:
                     return elem_city['destinationId'], elem_city['name']
 
+def hotel_info(destinationId,num):
+    '''def hotel_info - получаем информацию по отелям'''
+    url_list = "https://hotels4.p.rapidapi.com/properties/list"
+    querystring = {"destinationId": destinationId, "pageNumber": "1", "pageSize": "25", "checkIn": "2020-01-08",
+                   "checkOut": "2020-01-15", "adults1": "1", "sortOrder": "PRICE"}
+    response_list = requests.get(url_list, headers=headers, params=querystring)
+    response_new_list = json.loads(response_list.text)
+    result_list = response_new_list['data']['body']['searchResults']['results']
+    result_sort = sorted(result_list, key=lambda price: price['ratePlan']['price']['current'], reverse=False)
+    return result_sort[:num]
 
-# print (city_id('new'.capitalize()))
+def hotel_photo(photos_id_hotel,num):
+    ''' def hotel_photo -  получаем фотографии отеля'''
+    url_photos = "https://hotels4.p.rapidapi.com/properties/get-hotel-photos"
+    querystring = {"id": photos_id_hotel}
+    response_photos = requests.get(url_photos, headers=headers, params=querystring)
+    response__new_photos = json.loads(response_photos.text)
+    photos = response__new_photos['hotelImages']
+    random.shuffle(photos)
+    return photos[:num]
 
 
-
-
-
-
-# def get_data_first(message,hotel_info_dict):
-#     data_first = message.text
-#     try:
-#         data_first = datetime.strptime(data_first, "%Y-%m-%d")
-#         if data_first<datetime.today():
-#             new_data_first(message, hotel_info_dict)
-#         else:
-#             hotel_info_dict['data_first'] = data_first
-#             bot.send_message(message.from_user.id, f"Введите  дату выезда из отеля в формате ГГГГ-ММ-ДД")
-#             bot.register_next_step_handler(message, get_data_second, hotel_info_dict)
-#     except ValueError:
-#         ver_data_first(message,hotel_info_dict)
-#
-# def new_data_first(message,hotel_info_dict):
-#     bot.send_message(message.from_user.id, f"Нельзя ввести прошедшую дату или сегодняшнюю")
-#     bot.register_next_step_handler(message, get_data_first, hotel_info_dict)
-#
-# def ver_data_first(message,hotel_info_dict):
-#     bot.send_message(message.from_user.id, f"Нарушен формат даты ГГГГ-ММ-ДД (Пример - 2020-08-28")
-#     bot.register_next_step_handler(message, get_data_first, hotel_info_dict)
-#
-# def get_data_second(message, hotel_info_dict):
-#     data_second = message.text
-#     try:
-#         data_second = datetime.strptime(data_second, "%Y-%m-%d")
-#         if data_second<=hotel_info_dict['data_first']:
-#             new_data_second(message, hotel_info_dict)
-#         else:
-#             hotel_info_dict['data_second'] = data_second
-#             bot.send_message(message.from_user.id, f"Ищем отели")
-#             find_hotel(message, hotel_info_dict)
-#     except ValueError:
-#         ver_data_second(message,hotel_info_dict)
-#
-# def new_data_second(message,hotel_info_dict):
-#     bot.send_message(message.from_user.id, f"Дата заезда ментше даты выезда. Выберите другую дату выезда")
-#     bot.register_next_step_handler(message, get_data_second, hotel_info_dict)
-#
-# def ver_data_second(message,hotel_info_dict):
-#     bot.send_message(message.from_user.id, f"Нарушен формат даты")
-#     bot.register_next_step_handler(message, get_data_second, hotel_info_dict)
-
-def find_hotel(message,hotel_info_dict):
-    '''def find_hotel - вывод информации на экран
-    SEARCH - поиск ID города
-    LIST - поиск информации (включая ID) по отелю в необходимом нам городе
-    PHOTO - вывод фотографий отеля, использую ID отеля'''
-#     url_search = "https://hotels4.p.rapidapi.com/locations/v2/search"
-#     querystring_search = {"query": hotel_info_dict['city']}
-#     headers = {
-#         "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
-#         "X-RapidAPI-Key": os.getenv('APIKEY')
-#     }
-#     try:
-#         response_search = requests.get(url_search, headers=headers, params=querystring_search)
-#         response_new_search = json.loads(response_search.text)
-#         response_hotel_city_group = list(filter(lambda x: x['group'] == 'CITY_GROUP', response_new_search['suggestions']))
-#         destination_Id_filter =list(filter(lambda city: city['type'] == 'CITY', response_hotel_city_group[0]['entities']))
-#         new_destination_Id = [elem_city['destinationId']   for elem_city in destination_Id_filter if hotel_info_dict['city'][:3] in elem_city['caption'].replace(hotel_info_dict['city'][:3], '', 1) ]
-#
-#         url_list = "https://hotels4.p.rapidapi.com/properties/list"
-#         querystring_list = {"destinationId": new_destination_Id[0], "pageNumber": "1", "pageSize": "25",
-#                             "checkIn": "2020-01-08",
-#                             "checkOut": "2020-01-15", "adults1": "1", "sortOrder": "PRICE"}
-#         response_list = requests.get(url_list, headers=headers, params=querystring_list)
-#         response_new_list = json.loads(response_list.text)
-#         result_list = response_new_list['data']['body']['searchResults']['results']
-#         result_sort = sorted(result_list, key=lambda price: price['ratePlan']['price']['current'], reverse=False)
-#
-#         url_photos = "https://hotels4.p.rapidapi.com/properties/get-hotel-photos"
-#         for x in range(hotel_info_dict['count_hotel']):
-#             bot.send_message(message.from_user.id, f'''{x + 1}.Название отеля - {result_sort[x]['name']}
-# Адрес - {result_sort[x]['address']['countryName']},{result_sort[x]['address']['streetAddress'] if  'streetAddress'  in  result_sort[x]['address']  else 'Нет названия улицы'}
-# Расположение от центра  - {result_sort[x]['landmarks'][0]['distance']}
-# Цена  за ночь - {result_sort[x]['ratePlan']['price']['current']}
-# Цена за весь период {int(result_sort[x]['ratePlan']['price']['current'][1:]) * int((hotel_info_dict['data_second'] - hotel_info_dict['data_first']).days)}
-# ''')
-#             if hotel_info_dict['loading_photo'] == 'Да':
-#                 bot.send_message(message.from_user.id, 'Фото отеля')
-#                 photos_id_hotel = result_sort[x]['id']
-#                 querystring = {"id": photos_id_hotel}
-#                 response_photos = requests.get(url_photos, headers=headers, params=querystring)
-#                 response__new_photos = json.loads(response_photos.text)
-#                 photos = response__new_photos['hotelImages']
-#                 for photo in range(hotel_info_dict['count_photo']):
-#                     photo_url = photos[photo]['baseUrl']
-#                     photo_for_user = photo_url.replace('{size}', 'z')
-#                     bot.send_photo(message.from_user.id, photo_for_user)
-#
-#     except IndexError:
-#         bot.send_message(message.from_user.id, "Что-то пошло не так, уточните название города")
-#
-# bot.polling(none_stop=True, interval=0)
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import telebot
 import os
 # pip install python-telegram-bot-calendar
-from datetime import date, timedelta
+from datetime import date,timedelta,datetime
 from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
 import botrequests.lowprice
 
 bot = telebot.TeleBot(os.getenv('TOKEN'))
+user_dict = dict()
 max_count = 5
+
+
 
 @bot.message_handler(commands=['lowprice'])
 def first_command(message):
@@ -15,8 +18,9 @@ def first_command(message):
     bot.register_next_step_handler(message, get_city)
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['help'])
 def help(message):
+    '''def help - вызывает помощь по командам бота  '''
     bot.send_message(message.from_user.id, """1. Узнать топ самых дешёвых отелей в городе (команда /lowprice).
 2. Узнать топ самых дорогих отелей в городе (команда /highprice).
 3. Узнать топ отелей, наиболее подходящих по цене и расположению от центра
@@ -25,27 +29,27 @@ def help(message):
 
 @bot.message_handler(content_types=['text'])
 def help(message):
+    ''' def help - вызывает команду для помощи в управление командами бота'''
     bot.send_message(message.from_user.id, "Для перехода в меню напиши /help.")
 
 def get_city(message):
     '''def get_city - получаем название города где будем искать отели
     Делаем запрос на количество отелей выводимых на экран'''
     try:
-        hotel_info_dict = dict()
-        town = message.text.capitalize()
-        city_id, name_city = botrequests.lowprice.city_id(town)
+        city = message.text.capitalize()
+        city_id, name_city = botrequests.lowprice.city_id(city)
         if city_id is not None:
-            hotel_info_dict['city_id'] = city_id
+            user_dict[message.chat.id] = {'city_id': city_id}
             bot.send_message(message.from_user.id, f'Ищем отели в городе {name_city}')
             bot.send_message(message.from_user.id, "Какое количество отелей вывести на экран?(Максимальное количество - 5)")
-            bot.register_next_step_handler(message, get_count_hotel,hotel_info_dict)
+            bot.register_next_step_handler(message, get_count_hotel)
         else:
             bot.send_message(message.from_user.id, 'Поиск не распознаёт название города, повторите попытку')
             first_command(message)
     except ValueError:
         bot.send_message(message.from_user.id, "Что-то пошло не так")
 
-def get_count_hotel(message, hotel_info_dict):
+def get_count_hotel(message):
     '''def get_count_hotel - получаем количество отелей, коотрое выведем на экран пользователю
     А так же проверяем на минимальное и максимальное количество выводимых отелей на экран
     Минимальное количество - 1
@@ -54,44 +58,44 @@ def get_count_hotel(message, hotel_info_dict):
     try:
         count_hotel = int(message.text)
         if count_hotel > max_count:
-            hotel_info_dict['count_hotel'] = max_count
+            user_dict[message.chat.id].update({'count_hotel': max_count})
         elif count_hotel < 1:
-            hotel_info_dict['count_hotel']= 1
+            user_dict[message.chat.id].update({'count_hotel': 1})
         else:
-            hotel_info_dict['count_hotel']= count_hotel
+            user_dict[message.chat.id].update({'count_hotel': count_hotel})
         bot.send_message(message.from_user.id, "Загрузить фото для отеля?")
-        bot.register_next_step_handler(message, get_loading_photo, hotel_info_dict)
+        bot.register_next_step_handler(message, get_loading_photo)
     except ValueError:
-        verify_count_hotel(message, hotel_info_dict)
+        verify_count_hotel(message)
 
-def verify_count_hotel(message,hotel_info_dict):
+def verify_count_hotel(message):
     '''def verify_count_pnoto - верификация значения количество отелей
     На случай, когда пользователь вводит не число'''
     bot.send_message(message.from_user.id, "Необходимо ввести число от 1 до 5")
-    bot.register_next_step_handler(message, get_count_hotel,hotel_info_dict)
+    bot.register_next_step_handler(message, get_count_hotel)
 
-def get_loading_photo(message,hotel_info_dict):
+def get_loading_photo(message):
     '''def get_loading_photo - спрашивает у пользователя загружать фотографии или нет
      Делаем запрос на количествовывода фотографий отелей на экран, если ответ ДА
     Переходим к календарю для выбора даты заезда'''
     loading_photo = message.text.capitalize()
     if loading_photo == 'Да' or loading_photo == 'Нет':
-        hotel_info_dict['loading_photo'] = loading_photo
+        user_dict[message.chat.id].update({'loading_photo': loading_photo})
         if loading_photo == 'Да':
             bot.send_message(message.from_user.id, "Какое количество фотографий отеля вывести на экран?(Максимальное количество - 5)")
-            bot.register_next_step_handler(message, get_count_photos,hotel_info_dict)
+            bot.register_next_step_handler(message, get_count_photos)
         else:
-            calendar_check_in(message,hotel_info_dict)
+            set_date_in(message)
     else:
-        verify_loading_photo(message,hotel_info_dict)
+        verify_loading_photo(message)
 
-def verify_loading_photo(message,hotel_info_dict):
+def verify_loading_photo(message):
     '''def verify_loading_photo - верификация на ответ ДА или НЕТ
     Если ответ не совпадает, бот переспраштвает до тех пор пока не получит необходимый ответ'''
     bot.send_message(message.from_user.id, "ДА ли НЕТ")
-    bot.register_next_step_handler(message, get_loading_photo,hotel_info_dict)
+    bot.register_next_step_handler(message, get_loading_photo)
 
-def get_count_photos(message,hotel_info_dict):
+def get_count_photos(message):
     '''get_count_photos - получаем количество фотографий, которое выведем на экран
        Максимаотное коичество - 5
        Минимальное количество - 1
@@ -99,56 +103,114 @@ def get_count_photos(message,hotel_info_dict):
     try:
         count_photo = int(message.text)
         if count_photo > max_count:
-            hotel_info_dict['count_photo'] = max_count
+            user_dict[message.chat.id].update({'count_photo': max_count})
         elif count_photo < 1:
-            hotel_info_dict['count_photo'] = 1
+            user_dict[message.chat.id].update({'count_photo': 1})
         else:
-            hotel_info_dict['count_photo'] = count_photo
-        calendar_check_in(message,hotel_info_dict)
+            user_dict[message.chat.id].update({'count_photo': count_photo})
+        set_date_in(message)
     except ValueError:
-        verify_count_photo(message,hotel_info_dict)
+        verify_count_photo(message)
 
-def verify_count_photo(message,hotel_info_dict):
+def verify_count_photo(message):
     '''def verify_count_pnoto - верификация значения количества фотографий
     На случай, когда пользователь вводит не число'''
     bot.send_message(message.from_user.id, "Необходимо ввести число от 1 до 5")
-    bot.register_next_step_handler(message, get_count_photos,hotel_info_dict)
+    bot.register_next_step_handler(message, get_count_photos)
 
 
-def calendar_check_in(message,hotel_info_dict):
-    calendar, step = DetailedTelegramCalendar(calendar_id=1, min_date=date.today(),
-                                              max_date=date.today() + timedelta(days=90), locale='ru').build()
-    bot.send_message(message.chat.id, f"Заезд: Выберите {LSTEP[step]}", reply_markup=calendar)
+def set_date_in(message):
+    ''' def set_date_in - функция вызова даты заезда'''
+    bot.send_message(message.from_user.id, "Выберите дату заезда")
+    date_min = date.today()
+    date_max=date_min + timedelta(days=90)
+    calendar, step = DetailedTelegramCalendar(calendar_id=1, locale='ru', min_date=date_min, max_date=date_max).build()
+    bot.send_message(message.chat.id, f"Выберите {LSTEP[step]}", reply_markup=calendar)
+
+def set_date_input(chat_id, date_in):
+    ''' def set_date_input - записываем дату заезда'''
+    user_dict[chat_id].update({'date_in': date_in})
+
 
 @bot.callback_query_handler(func=DetailedTelegramCalendar.func(calendar_id=1))
-def cal1(c):
-    result, key, step = DetailedTelegramCalendar(calendar_id=1,min_date=date.today(),max_date=date.today()+timedelta(days=90), locale='ru').process(c.data)
+def cal(callback_query):
+    '''Функция обработчик календаря, выводит клавиатуру с календарем и ожидает ответ,
+    передает ответ в БД, и переходит к следующему шагу'''
+    date_min = date.today()
+    date_max = date.today() + timedelta(days=90)
+    result, key, step = DetailedTelegramCalendar(calendar_id=1, locale='ru', min_date=date_min, max_date=date_max).process(callback_query.data)
     if not result and key:
-        bot.edit_message_text(f"Заезд: Выберите {LSTEP[step]}",
-                              c.message.chat.id,
-                              c.message.message_id,
+        bot.edit_message_text(f"Выберите {LSTEP[step]}",
+                              callback_query.message.chat.id,
+                              callback_query.message.message_id,
                               reply_markup=key)
     elif result:
-        bot.edit_message_text(f"Вы выбрали  {result} дату заезда",
-                              c.message.chat.id,
-                              c.message.message_id)
+        bot.edit_message_text(f"Вы выбрали {result}",
+                              callback_query.message.chat.id,
+                              callback_query.message.message_id)
+        set_date_input(chat_id=callback_query.message.chat.id, date_in=str(result))
+        date_out(callback_query.message.chat.id)
 
-def calendar_check_out(message,hotel_info_dict):
-    calendar, step = DetailedTelegramCalendar(calendar_id=2, locale='ru').build()
-    bot.send_message(message.chat.id,
-                     f"Calendar 2: Выберите {LSTEP[step]}",
-                     reply_markup=calendar)
+def get_date_input(chat_id):
+    ''' def get_date_input - поллучаем дату заезда'''
+    return (user_dict[chat_id]['date_in'])
+
+
+def date_out(chat_id):
+    '''def date_out - функция вызова даты въезда     '''
+    date_today_min = (datetime.strptime(get_date_input(chat_id=chat_id), "%Y-%m-%d") + timedelta(
+        days=1)).date()
+    date_today_max = date_today_min + timedelta(days=90)
+    bot.send_message(chat_id, "Выберите дату выезда")
+    calendar, step = DetailedTelegramCalendar(calendar_id=2, locale='ru', min_date=date_today_min,max_date=date_today_max).build()
+    bot.send_message(chat_id, f"Выберите {LSTEP[step]}", reply_markup=calendar)
+
+def set_date_output(chat_id, date_out):
+    ''' def set_date_output - записываем дату выезда'''
+    user_dict[chat_id].update({'date_out': date_out})
+
 
 @bot.callback_query_handler(func=DetailedTelegramCalendar.func(calendar_id=2))
-def cal1(c):
-    result, key, step = DetailedTelegramCalendar(calendar_id=2, locale='ru').process(c.data)
+def cal(callback_query):
+    ''' def cal - функция обработчик календаря, выводит клавиатуру с календарем и ожидает ответ,
+    передает ответ в БД, и переходит к следующему шагу '''
+    date_today_min = (datetime.strptime(get_date_input(chat_id=callback_query.message.chat.id), "%Y-%m-%d") + timedelta(days=1)).date()
+    date_today_max = date_today_min + timedelta(days=90)
+    result, key, step = DetailedTelegramCalendar(calendar_id=2, locale='ru',
+                                                 min_date=date_today_min,max_date=date_today_max).process(callback_query.data)
     if not result and key:
-        bot.edit_message_text(f"Выезд: Выберите {LSTEP[step]}",
-                              c.message.chat.id,
-                              c.message.message_id,
+        bot.edit_message_text(f"Выберите {LSTEP[step]}",
+                              callback_query.message.chat.id,
+                              callback_query.message.message_id,
                               reply_markup=key)
     elif result:
-        bot.edit_message_text(f"Вы выбрали {result} дату выезда",
-                              c.message.chat.id,
-                              c.message.message_id)
+        bot.edit_message_text(f"Вы выбрали {result}",
+                              callback_query.message.chat.id,
+                              callback_query.message.message_id)
+        set_date_output(chat_id=callback_query.message.chat.id, date_out=str(result))
+        hotel_information(callback_query.message)
+
+def hotel_information(message):
+
+    ''' def hotel_information - вывод информации по отелям на экран пользователя
+    hotel_list - получаем список отелей
+    photo_list - получаем список фотографий отелей'''
+    hotel_list = botrequests.lowprice.hotel_info(user_dict[message.chat.id]['city_id'],max_count)
+    for hotel in range(user_dict[message.chat.id]['count_hotel']):
+        bot.send_message(message.chat.id, f'''{hotel + 1}.Название отеля - {hotel_list[hotel]['name']}
+        Адрес - {hotel_list[hotel]['address']['countryName']},{hotel_list[hotel]['address']['streetAddress'] if 'streetAddress' in hotel_list[hotel]['address'] else 'Нет названия улицы'}
+        Расположение от центра  - {hotel_list[hotel]['landmarks'][0]['distance']}
+        Цена  за ночь - {hotel_list[hotel]['ratePlan']['price']['current']} 
+        Цена за весь период {int(hotel_list[hotel]['ratePlan']['price']['current'][1:]) * 
+                             int(( datetime.strptime (user_dict[message.chat.id]['date_out'], "%Y-%m-%d") - datetime.strptime (user_dict[message.chat.id]['date_in'], "%Y-%m-%d")).days)}
+        ''')
+        if user_dict[message.chat.id]['loading_photo'] == 'Да':
+            bot.send_message(message.chat.id, 'Фото отеля')
+            photos_id_hotel = hotel_list[hotel]['id']
+            photo_list = botrequests.lowprice.hotel_photo(photos_id_hotel, max_count)
+            for photo in range(user_dict[message.chat.id]['count_photo']):
+                photo_url = photo_list[photo]['baseUrl']
+                photo_for_user = photo_url.replace('{size}', 'z')
+                bot.send_photo(message.chat.id, photo_for_user)
+
 bot.polling(none_stop=True, interval=0)
